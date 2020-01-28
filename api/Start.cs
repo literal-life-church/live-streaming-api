35e033ccd4fb2ff36ee92bac9637755129802879
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-01-27 20:39:35 -0500

    Wrapped up authentication into a class

--- api/Start.cs
+++ api/Start.cs
@@ -1,40 +1,26 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using LiteralLifeChurch.LiveStreamingApi.services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Management.Media;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Microsoft.Rest.Azure.Authentication;
-using LiteralLifeChurch.LiveStreamingApi.models;
-using LiteralLifeChurch.LiveStreamingApi.services;
-using Microsoft.Azure.Management.Media;
-using Microsoft.Rest;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace LiteralLifeChurch.LiveStreamingApi
 {
     public static class Start
     {
+        private static readonly AuthenticationService auth = new AuthenticationService();
+
         [FunctionName("Start")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            ConfigurationService configService = new ConfigurationService();
-            ConfigurationModel config = configService.GetConfiguration();
-
-            ClientCredential clientCredentials = new ClientCredential(config.ClientId, config.ClientSecret);
-            ServiceClientCredentials appCredentials = await ApplicationTokenProvider.LoginSilentAsync(config.TenantId, clientCredentials, ActiveDirectoryServiceSettings.Azure);
-
-            var client =  new AzureMediaServicesClient(config.ManagementEndpoint, appCredentials)
-            {
-                SubscriptionId = config.SubscriptionId,
-            };
+            AzureMediaServicesClient client = await auth.GetClientAsync();
 
             string name = req.Query["name"];
 
---
+++ api/services/AuthenticationService.cs
@@ -0,0 +1,27 @@
+﻿using LiteralLifeChurch.LiveStreamingApi.models;
+using Microsoft.Azure.Management.Media;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Rest.Azure.Authentication;
+using System.Threading.Tasks;
+
+namespace LiteralLifeChurch.LiveStreamingApi.services
+{
+    class AuthenticationService
+    {
+        private readonly ConfigurationService configService = new ConfigurationService();
+
+        public async Task<AzureMediaServicesClient> GetClientAsync()
+        {
+            ConfigurationModel config = configService.GetConfiguration();
+
+            ClientCredential clientCredentials = new ClientCredential(config.ClientId, config.ClientSecret);
+            ServiceClientCredentials appCredentials = await ApplicationTokenProvider.LoginSilentAsync(config.TenantId, clientCredentials, ActiveDirectoryServiceSettings.Azure);
+
+            return new AzureMediaServicesClient(config.ManagementEndpoint, appCredentials)
+            {
+                SubscriptionId = config.SubscriptionId,
+            };
+        }
+    }
+}
